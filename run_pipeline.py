de630db4fa4e8b0cc5ee538661c6f2f38b2b15f5
bkajj <muuciek.new@gmail.com>
2026-03-31 16:05:47 +0200

    refactor: use yesterday date in pipeline fetch function

--- run_pipeline.py
+++ run_pipeline.py
@@ -6,11 +6,13 @@ from requests.exceptions import RequestException
 from json import JSONDecodeError
 from fetch_data import fetch_data_from_api, FetchAPIException
 import logging
+import datetime as dt
+
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     logging.basicConfig(filename='pipeline.log', level=logging.INFO)
-
+    #todo: add timestamp to logs
     try:
         logger.info("Starting pipeline...")
         engine, Session = create_engine_and_session()
@@ -19,7 +21,8 @@ if __name__ == "__main__":
         init_db(engine)
 
         logger.info("Fetching data from API")
-        raw_data = fetch_data_from_api('AAPL', 'hour', '2026-03-05', '2026-03-13')
+        yesterday = dt.date.today() - dt.timedelta(days=1)
+        raw_data = fetch_data_from_api('AAPL', 'minute', yesterday, yesterday)
 
         logger.info("Saving data to database")
         load_to_db(raw_data, Session)
